f34dfd71991311079e2df4455af460a4d75d13cf
leedo <lee@laylward.com>
2010-07-28 16:06:36 -0500

    Added callback option, bump version

--- lib/HTML/Highlighter.pm
+++ lib/HTML/Highlighter.pm
@@ -5,12 +5,12 @@ use warnings;
 
 use HTML::Parser;
 use Plack::Request;
-use Plack::Util::Accessor qw/param/;
+use Plack::Util::Accessor qw/param callback/;
 
 use parent 'Plack::Middleware';
 
 use 5.008_001;
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 $VERSION = eval $VERSION;
 
 sub call {
@@ -20,27 +20,40 @@ sub call {
   return $res unless $res->[0] == 200 and $res->[2];
 
   my $h = Plack::Util::headers($res->[1]);
-  return unless $h->get("Content-Type") =~ /html/i;
+  return $res unless $h->get("Content-Type") =~ /html/i;
 
   my $req = Plack::Request->new($env);
-  my $highlight = $req->parameters->{ $self->param };
-  return $res unless $highlight;
+
+  $self->callback->($env) if $self->callback;
+
+  my $highlights = do {
+    if ($env->{'psgix.highlight'}) {
+      $env->{'psgix.highlight'};
+    } else {
+      $req->parameters->{ $self->param || "highlight" };
+    }
+  };
+
+  return $res unless $highlights;
+
+  my @highlights = split /\s+/, $highlights;
 
   my $html;
   my $p = HTML::Parser->new(
     api_version => 3,
     handlers => {
       default => [ sub { $html .= $_[0] }, "text" ],
-      text => [ sub { $_[0] =~ s/($highlight)/<span class="highlight">$1<\/span>/gi;
-                      $html .= $_[0] }, "text" ],
+      text => [ 
+        sub { for (@highlights) {
+                $_[0] =~ s/($_)/<span class="highlight">$1<\/span>/gi;
+              }
+              $html .= $_[0] }, "text" ],
       end_document => [ sub { $res->[2] = [$html];
                         $h->set('Content-Length' => length $html) }],
     }
   );
 
-  if (ref $res->[2] eq "CODE") {
-    $p->parse($res->[2]);
-  } elsif (ref $res->[2] eq "ARRAY") {
+  if (ref $res->[2] eq "ARRAY") {
     Plack::Util::foreach($res->[2], sub { $p->parse($_[0]) });
   } elsif ($res->[2]->can("getline")) {
     $p->parse_file($res->[2]);
@@ -61,8 +74,21 @@ HTML::Highlighter - PSGI middleware to highlight text in an HTML response
     use Plack::Builder;
     use HTML::Highlighter;
 
+    # highlight the "search" query param
+
     builder {
-      enable "+HTML::Highlighter", param => "highlight";
+      enable "+HTML::Highlighter", param => "search";
+      ...
+      $app;
+    };
+
+    # or highlight the user stored in session
+
+    builder {
+      enable "+HTML::Highlighter", callback => sub {
+        my $env = shift;
+        $env->{'psgix.highlight'} = $env->{'psgix.session'}{user};
+      };
       ...
       $app;
     };
@@ -70,9 +96,11 @@ HTML::Highlighter - PSGI middleware to highlight text in an HTML response
 =head1 DESCRIPTION
 
 The C<HTML::Highlighter> module is a piece of PSGI middleware that will inspect
-an HTML response and highlight parts of the page based on a query parameter. This
-is very much like what Google does when you load a page from their cache. Any
-text that matches your original query is highlighted.
+an HTML response and highlight parts of the page based on a query parameter or
+other request data. This is very much like what Google does when you load a
+page from their cache. Any text that matches your original query is highlighted.
+
+    <span class="highlight">[matching text]</span>
 
 This module also includes a javascript file called highlighter.js which gives
 you a class with methods to jump (scroll) through the highlights.
@@ -83,12 +111,18 @@ you a class with methods to jump (scroll) through the highlights.
 
 =item B<param>
 
-This required parameter allows you to specify what query parameter to use for
-the highlighting. For example, if you specify "highlight" as the param, each
-response will look for a query parameter called "highlight." If it finds the
-parameter, portions of the HTML response will be modified to look like this:
+This option allows you to specify a query parameter to be used for
+the highlighting. For example, if you specify "search" as the param, each
+response will look for a query parameter called "search" will highlight
+that value in the response.
 
-    <span class="highlight">[text]</span>
+=item B<callback>
+
+This option lets you specify a function that will be called on each request
+to generate the text used for highlighting. The function will be passed the
+$env hashref, and should set 'psgix.highlight' on it. This value will be used
+for the highlighting. This could be useful if you want to highlight a username
+that is stored in a session, or something similar.
 
 =back
 
--- t/01-highlight.t
+++ t/01-highlight.t
@@ -5,8 +5,11 @@ use Test::More;
 use Plack::Test;
 use HTTP::Request::Common;
 
-my $app = Plack::App::File->new(root => "t");
-$app = HTML::Highlighter->wrap($app, param => "highlight");
+my $file = Plack::App::File->new(root => "t");
+
+# get the highlight from a param
+#
+my $app = HTML::Highlighter->wrap($file, param => "highlight");
 
 test_psgi $app, sub {
   my $cb = shift;
@@ -20,5 +23,26 @@ test_psgi $app, sub {
     ok $res->content eq $html;
   }
 };
+
+# set highlight in callback
+#
+$app = HTML::Highlighter->wrap($file, callback => sub {
+    my $env = shift;
+    $env->{'psgix.highlight'} = "foo";
+});
+
+test_psgi $app, sub {
+  my $cb = shift;
+
+  my $res = $cb->(GET "/foo.html?highlight=foo");
+  is $res->code, 200;
+  {
+    local $/; 
+    open my $highlighted, '<', 't/foo-highlighted.html';
+    my $html = <$highlighted>;
+    ok $res->content eq $html;
+  }
+};
+
 
 done_testing();
